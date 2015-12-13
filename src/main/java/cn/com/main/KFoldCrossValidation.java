423097ec399804db360b29f13a2ce771c515974b
mcf171 <453372939@qq.com>
2015-12-13 11:24:53 +0800

    add some extra code to mark some dirty records
    add some extra code to mark some dirty records

--- src/main/java/cn/com/main/KFoldCrossValidation.java
+++ src/main/java/cn/com/main/KFoldCrossValidation.java
@@ -1,7 +1,10 @@
 package cn.com.main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import cn.com.model.Record;
 import cn.com.service.NaiveBayesian;
@@ -12,22 +15,36 @@ public class KFoldCrossValidation {
 	
 	public static void main(String args[]){
 		
+	
+		
 		int step = 4118;
 		double erroRate = 0;
 		List<Record> allRecord = TextUtil.getAllInformation("bank-additional-full.csv");
 		for(int i = 0 ; i < 10 ; i ++){
 			
 			System.out.println("iterator time is :" + (i+1));
-			List<Record> trainList = new ArrayList<Record>(allRecord.subList(i*step, i*step + step));
-			List<Record> preTestList =  new ArrayList<Record>(allRecord.subList(0, i*step));
-			List<Record> lastTestList =  new ArrayList<Record>(allRecord.subList(i*step + step, allRecord.size()));
-			preTestList.addAll(lastTestList);
-			List<Record> testList = preTestList;
+			
+			
+			List<Record> testList = new ArrayList<Record>(allRecord);
+			List<Record> trainList = new ArrayList<Record>();
+			Collections.copy(testList, allRecord);
+			for(int j = 0 ; j < step; j ++){
+				
+				Random random = new Random();
+		        int s = random.nextInt(testList.size())%(testList.size()-0+1) + 0;
+		        
+		        trainList.add(testList.get(s));
+		        testList.remove(s);
+			}
+			
 			
 			erroRate += NaiveBayesian.NaiveBayesianPredicte(trainList, testList);
 			
 		}
 		erroRate /=10;
-		System.out.println("total errorRate is : " + erroRate);
+		System.out.println("total errorRate is : " + erroRate*100 + "%");
+		
+		
+		
 	}
 }
