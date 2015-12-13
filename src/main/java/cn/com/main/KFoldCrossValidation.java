384ea735b89981f5dcc0c49011ca510838bf294e
mcf171 <453372939@qq.com>
2015-12-13 15:05:24 +0800

    add some comments
    add some comments

--- src/main/java/cn/com/main/KFoldCrossValidation.java
+++ src/main/java/cn/com/main/KFoldCrossValidation.java
@@ -19,13 +19,16 @@ public class KFoldCrossValidation {
 		
 		int step = 4118;
 		double erroRate = 0;
+		//提取所有的数据
 		List<Record> allRecord = TextUtil.getAllInformation("bank-additional-full.csv");
+		//k次迭代进行交叉检测
 		for(int i = 0 ; i < 10 ; i ++){
 			
 			System.out.println("iterator time is :" + (i+1));
 			
-			
+			//获取测试集合
 			List<Record> testList = new ArrayList<Record>(allRecord);
+			//获取训练集合
 			List<Record> trainList = new ArrayList<Record>();
 			Collections.copy(testList, allRecord);
 			for(int j = 0 ; j < step; j ++){
@@ -36,8 +39,7 @@ public class KFoldCrossValidation {
 		        trainList.add(testList.get(s));
 		        testList.remove(s);
 			}
-			
-			
+			//调用贝叶斯方法
 			erroRate += NaiveBayesian.NaiveBayesianPredicte(trainList, testList);
 			
 		}
